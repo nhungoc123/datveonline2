853b3517421de70a22316360b081ae8a768ef239
NhuNgoc <nhungoc2401@gmail.com>
2016-07-10 17:04:45 +0700

    Remove online font + analytics

--- data/views/include/head.php
+++ data/views/include/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-    <title>Movie Theater - Thế giới phim</title>
+    <title><?php echo WEBNAME ?> - Thế giới phim</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 
     <!-- <meta charset="utf-8"> -->
@@ -11,27 +11,31 @@
     <link rel="shortcut icon" type="image/png" href="<?php echo COMMON_DIR ?>images/favicon.png"/>
 
 <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,3516248,4,429,112,75,00011011']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3516248&101" alt="" border="0"></a></noscript>
+<script type="text/javascript">
+// var _Hasync= _Hasync|| [];
+// _Hasync.push(['Histats.start', '1,3516248,4,429,112,75,00011011']);
+// _Hasync.push(['Histats.fasi', '1']);
+// _Hasync.push(['Histats.track_hits', '']);
+// (function() {
+// var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+// hs.src = ('//s10.histats.com/js15_as.js');
+// (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+// })();
+</script>
+<noscript>
+<!-- <a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3516248&101" alt="" border="0"></a> -->
+</noscript>
 <!-- Histats.com  END  -->
 
 <!-- google -->
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-80514748-1', 'auto');
-  ga('send', 'pageview');
+  // ga('create', 'UA-80514748-1', 'auto');
+  // ga('send', 'pageview');
 </script>
 
     <link rel="stylesheet" href="<?php echo COMMON_DIR ?>css/bootstrap.css" rel="stylesheet"/>
@@ -43,10 +47,14 @@ hs.src = ('//s10.histats.com/js15_as.js');
     <link rel="stylesheet" href="<?php echo COMMON_DIR ?>css/fancy-buttons.css" />
     
     <!--=== Google Fonts ===-->
-    <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300,700,400' rel='stylesheet' type='text/css'>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Bangers' rel='stylesheet' type='text/css'> -->
+    <link href='<?php echo COMMON_DIR ?>fonts/bangers.css' rel='stylesheet' type='text/css'>
+    <!-- <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300,700,400' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:600,400,300' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'> -->
+    <link href='<?php echo COMMON_DIR ?>fonts/roboto_slab.css' rel='stylesheet' type='text/css'>
+    <link href='<?php echo COMMON_DIR ?>fonts/raleway.css' rel='stylesheet' type='text/css'>
+    <link href='<?php echo COMMON_DIR ?>fonts/open_sans.css' rel='stylesheet' type='text/css'>
     <!--=== Other CSS files ===-->
     <link rel="stylesheet" href="<?php echo COMMON_DIR ?>css/animate.css" />
     <link rel="stylesheet" href="<?php echo COMMON_DIR ?>css/jquery.vegas.css" />
@@ -62,14 +70,8 @@ hs.src = ('//s10.histats.com/js15_as.js');
     <!--=== Color Scheme, three colors are available red.css, orange.css and gray.css ===-->
     <link rel="stylesheet" id="scheme-source" href="<?php echo COMMON_DIR ?>css/schemes/gray.css" />
 
-
-    
     <link rel="stylesheet" href="<?php echo COMMON_DIR ?>css/checkout.css" />
     <!--=== Internet explorer fix ===-->
-    <!-- [if lt IE 9]>
-        <script src="http://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="http://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif] -->
 </head>
 
 <body>
